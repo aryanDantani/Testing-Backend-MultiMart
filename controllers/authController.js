b07420a3e7e24688aeac7274ecf4d4e7939e000b
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-18 21:37:29 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,13 +5,11 @@ const { validationResult } = require("express-validator");
 
 exports.signup = async (req, res) => {
   const { username, email, password } = req.body;
-
   // Validate input
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-
   try {
     // Check if user exists
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
@@ -20,10 +18,8 @@ exports.signup = async (req, res) => {
         .status(400)
         .json({ message: "Username or Email already exists" });
     }
-
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
-
     // Save user
     const newUser = new User({
       username,
@@ -32,7 +28,6 @@ exports.signup = async (req, res) => {
       role: "User",
     });
     await newUser.save();
-
     // Generate JWT token (optional)
     const token = jwt.sign(
       { userId: newUser._id, role: newUser.role },
@@ -41,7 +36,6 @@ exports.signup = async (req, res) => {
         expiresIn: "1h",
       }
     );
-
     res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
     console.error("Signup error:", error);
@@ -51,22 +45,18 @@ exports.signup = async (req, res) => {
 
 exports.signin = async (req, res) => {
   const { username, password } = req.body;
-
   try {
     // Find user
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
     // Compare passwords
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
-
     // Generate JWT
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-
     res.status(200).json({ message: "Login successful", token, user: user });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
