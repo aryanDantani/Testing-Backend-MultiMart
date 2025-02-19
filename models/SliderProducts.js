e6dd84840890a9fd2d73e1f2e977c831c69e3ad7
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-19 15:49:14 +0530

    Update SliderProducts.js

--- models/SliderProducts.js
+++ models/SliderProducts.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const sliderSchema = new mongoose.Schema(
+
   {
     title: { type: String, required: true },
     description: { type: String, required: true },
@@ -8,7 +9,9 @@ const sliderSchema = new mongoose.Schema(
     redirectUrl: { type: String },
     isActive: { type: Boolean, default: true },
   },
+
   { timestamps: true }
+
 );
 
 module.exports = mongoose.model("sliderproducts", sliderSchema);
