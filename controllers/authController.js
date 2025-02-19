260aaadcb7a37b77c124c0805bb256d3b1316727
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-19 15:48:04 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,13 +4,17 @@ const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
 
 exports.signup = async (req, res) => {
+
   const { username, email, password } = req.body;
+
   // Validate input
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+
   try {
+
     // Check if user exists
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
     if (existingUser) {
@@ -18,8 +22,10 @@ exports.signup = async (req, res) => {
         .status(400)
         .json({ message: "Username or Email already exists" });
     }
+
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
+
     // Save user
     const newUser = new User({
       username,
@@ -27,7 +33,9 @@ exports.signup = async (req, res) => {
       password: hashedPassword,
       role: "User",
     });
+
     await newUser.save();
+
     // Generate JWT token (optional)
     const token = jwt.sign(
       { userId: newUser._id, role: newUser.role },
@@ -37,28 +45,37 @@ exports.signup = async (req, res) => {
       }
     );
     res.status(201).json({ message: "User registered successfully", token });
+
   } catch (error) {
     console.error("Signup error:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
+
 };
 
 exports.signin = async (req, res) => {
+
   const { username, password } = req.body;
+
   try {
     // Find user
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
     // Compare passwords
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
+
     // Generate JWT
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
+
     res.status(200).json({ message: "Login successful", token, user: user });
+
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
+
 };
