de653625192c3843e1d96dbeba94c1e6afe073bf
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-19 15:49:42 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+
 const { signup, signin } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/signup', signup);
+
 router.post('/signin', signin);
 
 module.exports = router;
